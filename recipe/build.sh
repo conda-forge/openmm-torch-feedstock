82355cd06bbdd13f49b5aa909ec880f7fcd8aa33
peastman <peastman@stanford.edu>
2022-08-31 11:02:39 -0700

    Debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,14 @@ set -euxo pipefail
 
 rm -rf build || true
 
+ls ${PREFIX}/lib
+ls ${BUILD_PREFIX}/lib
+
 CMAKE_FLAGS="  -DCMAKE_INSTALL_PREFIX=${PREFIX}"
 CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
 CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
-CMAKE_FLAGS+=" -DOPENMM_DIR=${BUILD_PREFIX}"
+CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
 CMAKE_FLAGS+=" -DPYTORCH_DIR=${SP_DIR}/torch"
 CMAKE_FLAGS+=" -DTorch_DIR=${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch"
 # OpenCL
@@ -24,7 +27,11 @@ fi
 # Build in subdirectory and install.
 mkdir -p build
 cd build
-cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS="-undefined dynamic_lookup" ${SRC_DIR}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS="-undefined dynamic_lookup" ${SRC_DIR}
+else
+    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} ${SRC_DIR}
+fi
 make -j$CPU_COUNT install
 make -j$CPU_COUNT PythonInstall
 
