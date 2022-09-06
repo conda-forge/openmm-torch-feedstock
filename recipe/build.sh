dc6a2129a0b33a62ddb39854e9947f1d57ead720
peastman <peastman@stanford.edu>
2022-09-06 13:20:49 -0700

    Debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
 CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
 CMAKE_FLAGS+=" -DPYTORCH_DIR=${SP_DIR}/torch"
-#CMAKE_FLAGS+=" -DTorch_DIR=${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch"
+CMAKE_FLAGS+=" -DTorch_DIR=${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/torch/share/cmake/Torch"
 # OpenCL
 CMAKE_FLAGS+=" -DNN_BUILD_OPENCL_LIB=ON"
 CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=${PREFIX}/include"
@@ -24,11 +24,11 @@ fi
 # Build in subdirectory and install.
 mkdir -p build
 cd build
-#if [[ "$OSTYPE" == "darwin"* ]]; then
-#    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS_INIT="-undefined dynamic_lookup" ${SRC_DIR}
-#else
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS_INIT="-undefined dynamic_lookup" ${SRC_DIR}
+else
     cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} ${SRC_DIR}
-#fi
+fi
 make -j$CPU_COUNT install
 make -j$CPU_COUNT PythonInstall
 
@@ -43,7 +43,7 @@ fi
 cp -r tests ${PREFIX}/share/${PKG_NAME}/tests/
 ls -al ${PREFIX}/share/${PKG_NAME}/tests/
 
-#if [[ "$OSTYPE" == "darwin"* && $(arch) == 'arm64' ]]; then
-#    ${INSTALL_NAME_TOOL} -add_rpath @rpath/libtorch_cpu.dylib -add_rpath @rpath/libc10.dylib ${PREFIX}/lib/libOpenMMTorch.dylib
-#    otool -L ${PREFIX}/lib/libOpenMMTorch.dylib
-#fi
+if [[ "$OSTYPE" == "darwin"* && $(arch) == 'arm64' ]]; then
+    ${INSTALL_NAME_TOOL} -add_rpath @rpath/libtorch_cpu.dylib -add_rpath @rpath/libc10.dylib ${PREFIX}/lib/libOpenMMTorch.dylib
+    otool -L ${PREFIX}/lib/libOpenMMTorch.dylib
+fi
