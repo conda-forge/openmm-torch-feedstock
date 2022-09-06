fa2a3d11dfa4d02011da8a3346f787adfbe22c55
peastman <peastman@stanford.edu>
2022-09-06 10:46:47 -0700

    Debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -24,12 +24,11 @@ fi
 # Build in subdirectory and install.
 mkdir -p build
 cd build
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS_INIT="-undefined dynamic_lookup" ${SRC_DIR}
-else
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#    cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} -DCMAKE_SHARED_LINKER_FLAGS_INIT="-undefined dynamic_lookup" ${SRC_DIR}
+#else
     cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} ${SRC_DIR}
-fi
-cat CMakeCache.txt
+#fi
 make -j$CPU_COUNT install
 make -j$CPU_COUNT PythonInstall
 
@@ -44,7 +43,7 @@ fi
 cp -r tests ${PREFIX}/share/${PKG_NAME}/tests/
 ls -al ${PREFIX}/share/${PKG_NAME}/tests/
 
-if [[ "$OSTYPE" == "darwin"* && $(arch) == 'arm64' ]]; then
-    ${INSTALL_NAME_TOOL} -add_rpath @rpath/libtorch_cpu.dylib -add_rpath @rpath/libc10.dylib ${PREFIX}/lib/libOpenMMTorch.dylib
-    otool -L ${PREFIX}/lib/libOpenMMTorch.dylib
-fi
+#if [[ "$OSTYPE" == "darwin"* && $(arch) == 'arm64' ]]; then
+#    ${INSTALL_NAME_TOOL} -add_rpath @rpath/libtorch_cpu.dylib -add_rpath @rpath/libc10.dylib ${PREFIX}/lib/libOpenMMTorch.dylib
+#    otool -L ${PREFIX}/lib/libOpenMMTorch.dylib
+#fi
