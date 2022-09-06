50ecbfeefc9e5967e57a29351c74fbd8d7c9c3ad
peastman <peastman@stanford.edu>
2022-09-06 14:45:42 -0700

    Debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -44,6 +44,9 @@ cp -r tests ${PREFIX}/share/${PKG_NAME}/tests/
 ls -al ${PREFIX}/share/${PKG_NAME}/tests/
 
 if [[ "$OSTYPE" == "darwin"* && $(arch) == 'arm64' ]]; then
+    echo "Adding paths"
     ${INSTALL_NAME_TOOL} -add_rpath @rpath/libtorch_cpu.dylib -add_rpath @rpath/libc10.dylib ${PREFIX}/lib/libOpenMMTorch.dylib
     otool -L ${PREFIX}/lib/libOpenMMTorch.dylib
+else
+    echo "Not adding paths"
 fi
