9b682d9212ce5797bff239dcdbb273c850994282
Stefan Doerr <stefdoerr@gmail.com>
2025-02-12 16:57:43 +0200

    try different commands

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,14 @@ if exist build rd /s /q build
 mkdir build
 cd build
 
-:: Configure and build
-cmake %CMAKE_ARGS% %CMAKE_FLAGS% %SRC_DIR%
+
+cmake %SRC_DIR% -G Ninja -DCMAKE_INSTALL_PREFIX="%CONDA_PREFIX%\Library" -DTorch_DIR="%CONDA_PREFIX%\Lib\site-packages\torch\share\cmake\Torch" -DOPENMM_DIR="%CONDA_PREFIX%\Library" -DNN_BUILD_PYTHON_WRAPPERS=ON
 if errorlevel 1 exit 1
+
+:: check things with the config
+ccmake %SRC_DIR%
+cmake --build .
+cmake --install .
+
+:: manually fix setup.py torch_include_dirs line
+cmake --build . -- PythonInstall
